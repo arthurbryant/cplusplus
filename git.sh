4d531df4a6ed88afe29863582ce11bf3037fbd1e
arthur <arthur@ubuntu.ubuntu-domain>
2011-10-23 20:19:25 -0400

    add

--- git.sh
+++ git.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+git pull git@github.com:arthurbryant/cplusplus.git
 git add .
 git commit -m "add"
 git push
+
--- heap.cc
+++ heap.cc
@@ -1,101 +1,44 @@
 #include <iostream>
-#include <cstdlib>
+#include <algorithm>
+#include <vector>
+#include <iterator>
 
-#define minData -1;
+#define N 5
 
 using namespace std;
 
-typedef int ElementType;
-typedef struct HeapStruct* PQ;
-
-struct HeapStruct
+void print(vector<int> v)
 {
-    int size;
-    int capacity;
-    ElementType *Elements;
-};
-
-PQ init(int max)
-{
-    PQ H;
-    H = new HeapStruct;
-    if(NULL == H)
-    {
-        cerr << "heap overflow" << endl;
-        exit(-1);
-    }
-    H->Elements = new ElementType[max + 1];
-    H->size = 0;
-    H->capacity = max;
-    H->Elements[0] = minData;
-    
-    return H;
-}
-bool isFull(PQ H)
-{
-    return H->size >= H->capacity ? true : false;
-}
-void insert(ElementType x, PQ H)
-{
-    if(isFull(H))
-    {
-        cerr << "heap is full" << endl;
-        exit(-1);
-    }
-    int i;
-    for(i = ++H->size; x < H->Elements[i/2]; i/=2)
-        H->Elements[i] = H->Elements[i/2];
-    H->Elements[i] = x;
-}
-bool isEmpty(PQ H)
-{
-    return H->size <= 0 ? true : false;
-}
-void deleteMin(PQ H)
-{
-    if(isEmpty(H))
-    {
-        cerr << "heap is empty" << endl;
-        exit(-1);
-    }
-    ElementType last = H->Elements[H->size--];
-    cout << "last = " << last << endl;
-    int i, child;
-    for(i = 1; i*2 <= H->size; i = child)
-    {
-        child = i*2;
-        if(child != H->size && H->Elements[child+1] < H->Elements[child])
-            ++child;
-        if(last > H->Elements[child])
-            H->Elements[i] = H->Elements[child];
-        else
-            break;
-    }
-    H->Elements[i] = last;
-}
-void printHeap(PQ H)
-{
-    for(int i = 1; i <= H->size; ++i)
-        cout << H->Elements[i] << "\t";
-    cout << endl;
+		vector<int>::iterator iter = v.begin();
+		while(iter != v.end())
+			cout << *iter++ << " ";
+		cout << endl;
 }
 int main()
 {
-    int max = 10;
-    int arr[] = {6, 2, 5, 8, 1, 9, 3};
-    int len = sizeof(arr) / sizeof(int);
-    PQ H;
-    H = init(max);
-    for(int i = 0; i < len; ++i)
-        insert(arr[i], H);
-    printHeap(H);
-    deleteMin(H);
-    printHeap(H);
-    deleteMin(H);
-    printHeap(H);
+		int arr[] = {2, 4, 1, 3, 5};
+		vector<int> v(arr, arr+N);
+		make_heap(v.begin(), v.end());
+		cout << v.front() << endl;
+		print(v);
+		pop_heap(v.begin(), v.end());
+		print(v);
+		v.pop_back();
+		print(v);
+		cout << v.front() << endl;
+                v.push_back(99);
+		print(v);
+                sort_heap(v.begin(), v.end());
+		print(v);
+                int i;
+                for(i = 0; i < N; ++i)
+                {
+		        cout << v.front() << endl;
+		        pop_heap(v.begin(), v.end());
+		        v.pop_back();
+                        sort_heap(v.begin(), v.end());
+                    
+                }
 
-    delete[] H->Elements;
-    delete H;
-    
     return 0;
 }
